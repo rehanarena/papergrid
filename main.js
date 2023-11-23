88ee2e255ee098cc44bb6bfc7b6e976640dfdc00
rehanarena <150910796+rehanarena@users.noreply.github.com>
2023-11-23 12:13:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function sidenav(){
+    var dis=document.getElementById("toggle")
+    if(dis.style.display=="none"){
+        dis.style.display="block"
+    }else{
+        dis.style.display="none"
+    }
+}
